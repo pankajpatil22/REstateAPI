ced10a66bfcc094c8e8fc03d688fd9944bd98d33
Pankaj Patil <pankajpatil01998@gmail.com>
2024-03-27 15:42:50 +0530

    login backend

---
+++ REstate.API/REstate.Data/RestateContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace REstate.Data
+{
+    public class RestateContext : IdentityDbContext<IdentityUser>
+    {
+        public RestateContext(DbContextOptions<RestateContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
